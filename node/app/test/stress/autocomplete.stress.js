7b5bf61fc80f7276dfa63082f939a7ed7525f74d
Albert Gonzalez <albert.gonzalez@atrapalo.com>
2016-12-17 19:57:15 +0100

    stress test with hulken

---
+++ node/app/test/stress/autocomplete.stress.js
@@ -0,0 +1,23 @@
+const hulken = require('hulken');
+const randomWord = require('random-word');
+
+const requests = [];
+
+for(let i = 0; i < 200; i++) {
+    requests.push({
+        method: 'get',
+        path: `/api/games/autocomplete?q=${randomWord()}`
+    });
+}
+
+const hulken_options = {
+    targetUrl: 'http://localhost:8888',
+    requestsArray: JSON.stringify(requests),
+    numberOfHulkenAgents: 4
+};
+
+hulken.run(function(stats){
+    console.log('error ... perhaps i should look closer at the stats');
+},function(stats){
+    console.log('success! ... auto tweet my stats to the world!');
+},hulken_options);
